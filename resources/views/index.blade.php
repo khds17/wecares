cf1d64fdc1212b01fb9b30a14ce9ad7b3b8bb245
Kauan Henrique <khds2013@gmail.com>
2020-08-21 10:16:33 -0300

    Adicionando o novo template e removendo o bootstrap antigo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,59 @@
 @extends('templates.template')
 @section('content')
- WECARES CONTEÚDO
+    <div id="layoutDefault">
+            <div id="layoutDefault_content">
+                <main>
+                        <header class="page-header page-header-dark bg-gradient-primary-to-secondary">
+                            <div class="page-header-content pt-10">
+                                <div class="container">
+                                    <div class="row align-items-center">
+                                        <div class="col-lg-6" data-aos="fade-up">
+                                            <h1 class="page-header-title">Build your next project faster with SB UI Kit Pro</h1>
+                                            <p class="page-header-text mb-5">Welcome to SB UI Kit Pro, a toolkit for building beautiful web interfaces, created by the developmet team at Start Bootstrap</p>
+                                            <a class="btn btn-teal btn-marketing rounded-pill lift lift-sm" href="landing-multipurpose.html">View Demo<i class="fas fa-arrow-right ml-1"></i></a><a class="btn btn-link btn-marketing" href="https://docs.startbootstrap.com/sb-ui-kit-pro/quickstart">Documentation</a>
+                                        </div>
+                                        <div class="col-lg-6 d-none d-lg-block" data-aos="fade-up" data-aos-delay="50"><img class="img-fluid" src="assets/img/drawkit/color/drawkit-content-man-color.svg" /></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="svg-border-rounded text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
+                            </div>
+                        </header>
+                        <section class="bg-white py-10">
+                            <div class="container">
+                                <div class="row text-center">
+                                        Conteúdo
+                                </div>
+                            </div>
+                            <div class="svg-border-rounded text-light">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
+                            </div>
+                        </section>
+                        <section class="bg-light py-10">
+                            <div class="container">
+                                Conteudo
+                            <div class="svg-border-rounded text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
+                            </div>
+                        </section>
+                        <section class="bg-white py-10">
+                            <div class="container">
+                                Conteúdo
+                            </div>
+                            <div class="svg-border-rounded text-light">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
+                            </div>
+                        </section>
+                        <section class="bg-light py-10">
+                            <div class="container">
+                                Conteúdo
+                            </div>
+                            <div class="svg-border-rounded text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
+                            </div>
+                        </section>
+            </main>
+        </div>
+    </div>
 @endsection
