9a0cc48402bd4454b924310078b231ab93b52e0e
Kauan Henrique <khds2013@gmail.com>
2020-09-26 08:32:46 -0300

    Mudando layout dos termos

--- resources/views/index/conduta.blade.php
+++ resources/views/index/conduta.blade.php
@@ -1,12 +1,24 @@
 @extends('templates.template')
 @section('content')
+<header class="page-header page-header-dark bg-gradient-primary-to-secondary">
+    <div class="page-header-content pt-10">
+        <div class="container text-center">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <h1 class="page-header-title mb-3">Terms &amp; Privacy</h1>
+                        <p class="page-header-text">Last updated: January 2020</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="svg-border-rounded text-light">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
+    </div>
+</header>
 <section class="bg-light py-10">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-10 mt-5">
-                <h1>Terms &amp; Privacy</h1>
-                <p class="lead">Last updated: January 2020</p>
-                <hr class="my-5" />
                 <h2 class="mb-3">1. Overview</h2>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, corporis? Facere quisquam earum dolore excepturi minima, fuga laborum reiciendis molestias nesciunt, impedit perspiciatis voluptatem nisi ad modi labore ipsam quas.</p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, quis explicabo? Quis non minima, placeat aspernatur reiciendis, itaque quam quos eaque doloribus vero facere libero enim quo dolor. Et, fugit!</p>
