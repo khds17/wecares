fe26bb08e14ca8bff60a2f73a9da3f903ccbbab3
Kauan Henrique <khds2013@gmail.com>
2020-10-14 19:03:53 -0300

    add uma nova tabela no bd

--- database/migrations/2020_09_06_132952_create_servicos_table.php
+++ database/migrations/2020_09_06_132952_create_servicos_table.php
@@ -15,26 +15,7 @@ class CreateServicosTable extends Migration
     {
         Schema::create('SERVICOS', function (Blueprint $table) {
             $table->increments('ID');
-            $table->unsignedInteger('ID_PRESTADOR');
-            $table->unsignedInteger('ID_SOLICITANTE');
-            $table->unsignedInteger('ID_PACIENTE');
-            $table->string('NOME',255);
-            $table->string('TIPO', 25);
-            $table->string('LOCALIZACAO', 25);
-            $table->unsignedInteger('ID_ENDERECO');
-            $table->string('SERVICOS', 255);
-            $table->unsignedInteger('MEDICAMENTOS');
-            $table->string('TIPO_MEDICAMENTOS');
-            $table->date('DATA_SERVICO');
-            $table->time('HORA_INICIO',0);
-            $table->time('HORA_FIM',0);
-            $table->unsignedSmallInteger('STATUS');
-            $table->float('VALOR',8,2);
-            $table->foreign('ID_PRESTADOR')->references('ID')->on('PRESTADORES')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('ID_SOLICITANTE')->references('ID')->on('SOLICITANTES')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('ID_PACIENTE')->references('ID')->on('PACIENTES')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('ID_ENDERECO')->references('ID')->on('ENDERECOS')->onDelete('cascade')->onUpdate('cascade');            
-  
+            $table->string('TIPO', 255);  
         });
     }
 
--- database/migrations/2020_10_14_193618_create_servicos_pretados_table.php
+++ database/migrations/2020_10_14_193618_create_servicos_pretados_table.php
@@ -13,9 +13,27 @@ class CreateServicosPretadosTable extends Migration
      */
     public function up()
     {
-        Schema::create('servicos_pretados', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create('SERVICOS_PRESTADOS', function (Blueprint $table) {
+            $table->increments('ID');
+            $table->unsignedInteger('ID_PRESTADOR');
+            $table->unsignedInteger('ID_SOLICITANTE');
+            $table->unsignedInteger('ID_PACIENTE');
+            $table->string('NOME',255);
+            $table->string('TIPO', 25);
+            $table->string('LOCALIZACAO', 25);
+            $table->unsignedInteger('ID_ENDERECO');
+            $table->string('SERVICOS', 255);
+            $table->unsignedInteger('MEDICAMENTOS');
+            $table->string('TIPO_MEDICAMENTOS');
+            $table->date('DATA_SERVICO');
+            $table->time('HORA_INICIO',0);
+            $table->time('HORA_FIM',0);
+            $table->unsignedSmallInteger('STATUS');
+            $table->float('VALOR',8,2);
+            $table->foreign('ID_PRESTADOR')->references('ID')->on('PRESTADORES')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('ID_SOLICITANTE')->references('ID')->on('SOLICITANTES')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('ID_PACIENTE')->references('ID')->on('PACIENTES')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('ID_ENDERECO')->references('ID')->on('ENDERECOS')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
