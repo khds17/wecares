134b923d64895fed803e53a5da4e35ab5fd3d1cd
Kauan Henrique <khds2013@gmail.com>
2020-10-15 13:54:03 -0300

    Add mensagens da validação do formulario

--- app/Http/Requests/requestPrestador.php
+++ app/Http/Requests/requestPrestador.php
@@ -33,7 +33,7 @@ class requestPrestador extends FormRequest
             'prestadorEmail'=>'bail|required|email:rfc,dns|unique:PRESTADORES,EMAIL',
             'prestadorSenha'=>'required:min:8',
             'prestadorConfirmarSenha'=>'required|min:8|same:prestadorSenha',
-            'prestadorCep'=>'bail|required|min:8|max:9',
+            'prestadorCep'=>'bail|required|min:8|max:8',
             'prestadorEndereco'=>'required',
             'prestadorNumero'=>'bail|required|numeric',
             'prestadorCidade'=>'required',
@@ -46,10 +46,42 @@ class requestPrestador extends FormRequest
         ];
     }
     //Mensagens personalizadas de retorno
-    // public function messages()
-    // {
-    //     return[
-    //         'prestadorNome.required'=>'Nome é obrigatório',
-    //     ];
-    // }
+    public function messages()
+    {
+        return[
+            'prestadorNome.required'=>'Nome é um campo obrigatório',
+            'prestadorNome.mix'=>'Preencha o nome completo',
+            'prestadorCPF.required'=>'CPF é um campo obrigatório',
+            'prestadorCPF.unique'=>'CPF já cadastrado. Caso seja o dono deste CPF, entre em contato conosco!',
+            'prestadorCPF.min'=>'CPF inválido',
+            'prestadorCPF.max'=>'CPF inválido',
+            'prestadorNascimento.required'=>'Data de nascimento é um campo obrigatório',
+            'prestadorNascimento.date'=>'Data inválida',
+            'sexo.required'=>'Sexo é um campo obrigatório',
+            'prestadorEmail.required'=>'E-mail é um campo obrigatório',
+            'prestadorEmail.email'=>'E-mail inválido',
+            'prestadorEmail.unique'=>'E-mail já cadastrado. Caseo seja o dono deste e-mail, entre em contato conosco!',
+            'prestadorSenha.required'=>'Senha é um campo obrigatório',
+            'prestadorSenha.min'=>'Digite uma senha de no mínimo 8 digitos',
+            'prestadorConfirmarSenha.required'=>'Confirme a senha é um campo obrigatório',
+            'prestadorConfirmarSenha.min'=>'Digite uma senha de no mínimo 8 digitos',
+            'prestadorConfirmarSenha.same'=>'As senhas são diferentes',
+            'prestadorCep.required'=>'CEP é um campo obrigatório',
+            'prestadorCep.min'=>'CEP inválido',
+            'prestadorCep.max'=>'CEP inválido',
+            'prestadorEndereco.required'=>'Endereço é um campo obrigatório',
+            'prestadorNumero.required'=>'Número é um campo obrigatório',
+            'prestadorNumero.numeric'=>'Número só aceita valores númericos',
+            'prestadorCidade.required'=>'Cidade é um campo obrigatório',
+            'prestadorBairro.required'=>'Bairro é um campo obrigatório',
+            'prestadorEstado.required'=>'Estado é um campo obrigatório',
+            'formacao.required'=>'Formação é um campo obrigatório',
+            'certificadoFormacao.required'=>'Certificado é um documento obrigatório',
+            'certificadoFormacao.mimes'=>'Só são aceitos arquivos JPEG, PNG ou PDF',
+            'antecedentes.required'=>'Antecedentes é um documento obrigatório',
+            'antecedentes.mimes'=>'Só são aceitos arquivos JPEG, PNG ou PDF',
+            'termos.required'=>'Termos e aceite são obrigatórios'
+
+        ];
+    }
 }
