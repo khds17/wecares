578a6b2ccada21bc8fb57f0bdfd95993e01d499d
Kauan Henrique <khds2013@gmail.com>
2020-10-17 07:17:29 -0300

    Configurando o model para conseguir dar select, nunca esqueca essa porra

--- app/Models/formacao.php
+++ app/Models/formacao.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class formacao extends Model
 {
-    //
+    public $timestamps = false;
+    protected $table='FORMACAO';
+    protected $fillable=['ID','FORMACAO'];
 }
--- app/Models/sexo.php
+++ app/Models/sexo.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class sexo extends Model
 {
-    //
+    public $timestamps = false;
+    protected $table='SEXOS';
+    protected $fillable=['ID','SEXO'];
 }
