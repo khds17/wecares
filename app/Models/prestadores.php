c3f9bc5d2a10ab70fea5f9c543f2f2839f834fed
Kauan Henrique <khds2013@gmail.com>
2020-10-17 05:25:46 -0300

    Mexendo no model

--- app/Models/prestadores.php
+++ app/Models/prestadores.php
@@ -9,7 +9,7 @@ class prestadores extends Model
     public $timestamps = false;
     protected $table='PRESTADORES';
     //
-    protected $fillable=['NOME','CPF','TELEFONE','DT_NASCIMENTO','SEXO','EMAIL','SENHA','ID_ENDERECO','FORMACAO','ID_CERTIFICADO','ID_ANTEDECENTE','STATUS'];
+    protected $fillable=['NOME','CPF','TELEFONE','DT_NASCIMENTO','SEXO','EMAIL','SENHA','ID_ENDERECO','FORMACAO','ID_CERTIFICADO','ID_ANTECEDENTE','STATUS','ID_ACESSO'];
 
     // Criando relacionamento entre as tabelas prestadores e enderecos
     public function relEndereco(){
