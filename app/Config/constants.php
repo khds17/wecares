09e20290fe61fb3ef8f62d6c92d495320789bc9c
Kauan Henrique <khds2013@gmail.com>
2020-10-19 07:10:08 -0300

    identacao

--- app/Config/constants.php
+++ app/Config/constants.php
@@ -2,14 +2,14 @@
 
 return [
 
-    'STATUS'=>[
+    'STATUS' => [
         'PENDENTE' => 0,
         'ATIVO' => 1,
         'REPROVADO' => 2,
         'DESATIVADO' => 3
     ],
 
-    'ACESSOS'=>[
+    'ACESSOS' => [
         'PRESTADOR' => 1,
         'SOLICITANTE' => 2
     ]
