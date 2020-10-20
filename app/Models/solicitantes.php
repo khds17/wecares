135a1c8c17cfcdfd5a9b63b2c8a764546938322e
Kauan Henrique <khds2013@gmail.com>
2020-10-20 18:47:29 -0300

    mexendo no model do solicitante

--- app/Models/solicitantes.php
+++ app/Models/solicitantes.php
@@ -8,10 +8,12 @@ class solicitantes extends Model
 {
     public $timestamps = false;
     protected $table='SOLICITANTES';
-    protected $fillable=['NOME','CPF','EMAIL','TELEFONE','SENHA','ID_ENDERECO','TIPO_FAMILIAR','TIPO_FAMILIAR_OUTROS','STATUS'];
+    protected $fillable=['NOME','CPF','EMAIL','TELEFONE','SENHA','ID_ENDERECO','ID_FAMILIARIDADE','FAMILIAR_OUTROS','STATUS'];
 
-    // public $timestamps = false;
-    // protected $table='SOLICITANTE';
-    // protected $fillable=[''];
+        // Criando relacionamento entre as tabelas solicitantes e enderecos
+        public function relEndereco(){
+            return $this->hasOne('App\Models\enderecos', 'ID','ID_ENDERECO');
+        }
+
 }
 
