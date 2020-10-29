b1c0e365bda539bf22a9ed4fac237121d01d1506
Kauan Henrique <khds2013@gmail.com>
2020-10-29 09:15:07 -0300

    Coloquei o csrf no cabecalho do template admin

--- resources/views/templates/template-admin.blade.php
+++ resources/views/templates/template-admin.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
         <title>WeCares</title>
         <!-- Custom fonts for this template-->
         <link href="{{url('assets/layout-admin/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
@@ -350,7 +351,7 @@
 
         <!-- Core plugin JavaScript-->
         <script src="{{url('assets/layout-admin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <!-- Custom scripts for all pages-->
         <script src="{{url('assets/layout-admin/js/sb-admin-2.min.js')}}"></script>
         <script src="{{url('assets/layout-admin/js/javascript.js')}}"></script>
