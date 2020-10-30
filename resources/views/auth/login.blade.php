84acc740ef89525950eb269fae6f62f6c035f3e8
Kauan Henrique <khds2013@gmail.com>
2020-10-29 21:08:53 -0300

    Publicando o login, porem precisara mexer no html para melhorar o visual

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
                     <div class="card rounded-lg text-dark">
                         <div class="card-header py-4">Entrar</div>
                         <div class="card-body">
-                            <form class="user">
+                            {{-- <form class="user">
                                 @csrf
                                 <div class="form-group">
                                     <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="E-mail">
@@ -24,7 +24,7 @@
                                 </div>
                                 <a href="index.html" class="btn btn-primary btn-user btn-block">
                                 Login
-                                </a>
+                                </a> --}}
                                 {{-- <hr>
                                 <a href="index.html" class="btn btn-google btn-user btn-block">
                                     <i class="fab fa-google fa-fw"></i> Login com Google
@@ -32,6 +32,50 @@
                                 <a href="index.html" class="btn btn-facebook btn-user btn-block">
                                     <i class="fab fa-facebook-f fa-fw"></i> Login com Facebook
                                 </a> --}}
+                            {{-- </form> --}}
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+        
+                                <div class="form-group">   
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="E-mail" autofocus>
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                </div>
+        
+                                <div class="form-group">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Senha">
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                </div>
+        
+                                <div class="form-group">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                            <label class="form-check-label" for="remember">
+                                                {{ __('Lembrar-me') }}
+                                            </label>
+                                        </div>
+                                </div>
+        
+                                <div class="form-group">
+                                    <div class="col-md-8 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('Login') }}
+                                        </button>
+        
+                                        @if (Route::has('password.request'))
+                                            <a class="btn-white" href="{{ route('password.request') }}">
+                                                {{ __('Esqueceu a senha?') }}
+                                            </a>
+                                        @endif
+                                    </div>
+                                </div>
                             </form>
                         </div>
                     </div>
