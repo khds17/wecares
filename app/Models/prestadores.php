d6dc375805e3bc8ba007e6c1b2b2e296904980bd
Kauan Henrique <khds2013@gmail.com>
2020-11-11 06:01:51 -0300

    Corrigindo relacionamento no model

--- app/Models/prestadores.php
+++ app/Models/prestadores.php
@@ -17,7 +17,7 @@ class prestadores extends Model
 
     // Criando relacionamento entre as tabelas solicitantes e users
     public function relUsuario(){
-        return $this->hasOne('App\Models\users', 'ID','ID_USUARIO');
+        return $this->hasOne('App\Models\user', 'ID','ID_USUARIO');
     }
 
     // Criando relacionamento entre as tabelas prestadores e enderecos
