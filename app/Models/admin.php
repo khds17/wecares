525957fba9ee3eab588e365a4c9e1806b037704c
Kauan Henrique <khds2013@gmail.com>
2020-11-13 06:41:05 -0300

    Criando relacionamento da tabela admin com users

--- app/Models/admin.php
+++ app/Models/admin.php
@@ -9,4 +9,9 @@ class admin extends Model
     public $timestamps = false;
     protected $table='ADMIN';
     protected $fillable=['NOME','EMAIL','SENHA','ID_USUARIO','STATUS'];
+
+    // Criando relacionamento entre as tabelas admin e users
+    public function relUsuario(){
+        return $this->hasOne('App\Models\user', 'ID','ID_USUARIO');
+    }
 }
