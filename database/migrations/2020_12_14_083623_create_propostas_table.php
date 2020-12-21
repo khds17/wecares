f55e1f2be2e74befdfdfee6e09a88ceb22d49b05
Kauan Henrique <khds2013@gmail.com>
2020-12-21 07:44:40 -0300

    Alterei a estrutura da tabela de propostas

--- database/migrations/2020_12_14_083623_create_propostas_table.php
+++ database/migrations/2020_12_14_083623_create_propostas_table.php
@@ -16,27 +16,33 @@ class CreatePropostasTable extends Migration
         Schema::create('PROPOSTAS', function (Blueprint $table) {
             $table->increments('ID');
             $table->unsignedInteger('ID_PRESTADOR');
+            $table->string('NOME_PRESTADOR',255);
             $table->unsignedInteger('ID_SOLICITANTE');
+            $table->string('NOME_SOLICITANTE',255);
             $table->unsignedInteger('ID_PACIENTE');
-            $table->string('NOME',255);
+            $table->string('NOME_PACIENTE',255);
             $table->string('TIPO', 25);
             $table->string('LOCALIZACAO', 25);
-            $table->unsignedInteger('ID_ENDERECO');
-            $table->unsignedInteger('ID_SERVICOS');
+            $table->string('CEP',13);
+            $table->string('ENDERECO',255);
+            $table->integer('NUMERO');
+            $table->string('COMPLEMENTO',255)->nullable();
+            $table->string('BAIRRO',50);
+            $table->string('CIDADE',50);
+            $table->string('UF',2);
+            $table->string('SERVICOS');
             $table->string('SERVICOS_OUTROS', 255)->nullable();
-            $table->unsignedInteger('MEDICAMENTOS');
+            $table->string('TOMA_MEDICAMENTOS',3);
             $table->string('TIPO_MEDICAMENTOS')->nullable();
             $table->date('DATA_SERVICO');
             $table->time('HORA_INICIO',0);
             $table->time('HORA_FIM',0);
-            $table->unsignedSmallInteger('APROVACAO_SOLICITANTE');
-            $table->unsignedSmallInteger('APROVACAO_PRESTADOR');
+            $table->unsignedSmallInteger('APROVACAO_SOLICITANTE')->nullable();
+            $table->unsignedSmallInteger('APROVACAO_PRESTADOR')->nullable();
             $table->float('VALOR',8,2);
             $table->foreign('ID_PRESTADOR')->references('ID')->on('PRESTADORES')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('ID_SOLICITANTE')->references('ID')->on('SOLICITANTES')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('ID_PACIENTE')->references('ID')->on('PACIENTES')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('ID_ENDERECO')->references('ID')->on('ENDERECOS')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('ID_SERVICOS')->references('ID')->on('SERVICOS')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
