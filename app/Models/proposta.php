d6e1c9a2323f68d7e021b5528ce5d777be126b27
Kauan Henrique <khds2013@gmail.com>
2021-01-04 14:48:51 -0300

    Melhorando a formatacao

--- app/Models/proposta.php
+++ app/Models/proposta.php
@@ -8,5 +8,34 @@ class proposta extends Model
 {
     public $timestamps = false;
     protected $table='PROPOSTAS';
-    protected $fillable=['ID_PRESTADOR','NOME_PRESTADOR','ID_SOLICITANTE','NOME_SOLICITANTE','ID_FAMILIARIDADE','OUTROS_FAMILIARIDADE','ID_PACIENTE','NOME_PACIENTE','TIPO','LOCALIZACAO','CEP','ENDERECO','NUMERO','COMPLEMENTO','BAIRRO','CIDADE','UF','SERVICOS','SERVICOS_OUTROS','TOMA_MEDICAMENTOS','TIPO_MEDICAMENTOS','DATA_SERVICO','HORA_INICIO','HORA_FIM','APROVACAO_SOLICITANTE','APROVACAO_PRESTADOR','VALOR'];
+    protected $fillable=[
+        'ID_PRESTADOR',
+        'NOME_PRESTADOR',
+        'ID_SOLICITANTE',
+        'NOME_SOLICITANTE',
+        'ID_FAMILIARIDADE',
+        'OUTROS_FAMILIARIDADE',
+        'ID_PACIENTE',
+        'NOME_PACIENTE',
+        'TIPO',
+        'LOCALIZACAO',
+        'CEP',
+        'ENDERECO',
+        'NUMERO',
+        'COMPLEMENTO',
+        'BAIRRO',
+        'CIDADE',
+        'UF',
+        'SERVICOS',
+        'SERVICOS_OUTROS',
+        'TOMA_MEDICAMENTOS',
+        'TIPO_MEDICAMENTOS',
+        'DATA_SERVICO',
+        'HORA_INICIO',
+        'HORA_FIM',
+        'APROVACAO_SOLICITANTE',
+        'APROVACAO_PRESTADOR',
+        'VALOR'
+    ];
 }
+
