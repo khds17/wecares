c49bcf47ca060d1d72e9765f2789bd81558ac702
Kauan Henrique <khds2013@gmail.com>
2021-01-26 11:17:38 -0300

    Mexendo na pagina sobre

--- resources/views/index/sobre.blade.php
+++ resources/views/index/sobre.blade.php
@@ -1,12 +1,12 @@
 @extends('templates.template')
 @section('content')
-{{-- <header class="page-header page-header-dark bg-gradient-primary-to-secondary">
+<header class="page-header page-header-dark bg-gradient-primary-to-secondary">
     <div class="page-header-content pt-10">
         <div class="container text-center">
             <div class="row justify-content-center">
                 <div class="col-lg-8">
                     <h1 class="page-header-title mb-3">WeCares</h1>
-                        <p class="page-header-text">Our company culture, and how we do things</p>
+                        <p class="page-header-text">A cultura da nossa empresa e como fazemos as coisas</p>
                 </div>
             </div>
         </div>
@@ -14,7 +14,7 @@
     <div class="svg-border-rounded text-light">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
     </div>
-</header> --}}
+</header>
 <section class="bg-light py-10">
     <div class="container">
         <div class="row justify-content-center">
