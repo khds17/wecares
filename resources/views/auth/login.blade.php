f28c47fdabd6f55db04e20f3c1617fb8f470c0fa
Kauan Henrique <khds2013@gmail.com>
2021-01-27 09:04:18 -0300

    Comentei o lembra-me para fazer depois

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,14 +54,14 @@
                                         @enderror
                                 </div>
         
-                                <div class="form-group">
+                                {{-- <div class="form-group">
                                         <div class="form-check">
                                             <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                             <label class="form-check-label" for="remember">
                                                 {{ __('Lembrar-me') }}
                                             </label>
                                         </div>
-                                </div>
+                                </div> --}}
                                 <div class="form-group">
                                     <div class="col-md-8 offset-md-4">
                                         <button type="submit" class="btn btn-primary">
