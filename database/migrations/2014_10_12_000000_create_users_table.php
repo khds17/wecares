4953aea0265979036da7e06fac3e2a09dc27bbac
Kauan Henrique <khds2013@gmail.com>
2021-01-29 16:04:14 -0300

    Removendo uma condicao na criacao do campo, pois estava prejudicando as migrations ao subir na producao

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email',255)->unique();
+            $table->string('email',255);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedInteger('status');
