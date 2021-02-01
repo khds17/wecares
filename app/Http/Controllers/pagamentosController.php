c9943a46a4355afa87217bd4389fd9f41eed1024
Kauan Henrique <khds2013@gmail.com>
2021-02-01 20:38:05 -0300

    Deu ruim no teste, mexendo pra dar certo

--- app/Http/Controllers/pagamentosController.php
+++ app/Http/Controllers/pagamentosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\solicitantes;
+use Illuminate\Support\Facades\DB;
 // use App\Models\cartoes;
 
 class pagamentosController extends Controller
