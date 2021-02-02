513b8d55c46e17b0c6d42d751289bf0aff3ebf76
Kauan Henrique <khds2013@gmail.com>
2021-02-02 11:52:59 -0300

    Eu estava fazendo merda

--- app/Models/valida_cartao.php
+++ app/Models/valida_cartao.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class valida_cartao extends Model
 {
     public $timestamps = false;
-    protected $table='PAGAMENTOS';
+    protected $table='VALIDA_CARTAO';
     protected $fillable=['ID_PAGAMENTO','ID_CARTAO','STATUS','DT_CRIACAO','DT_APROVACAO'];
 }
