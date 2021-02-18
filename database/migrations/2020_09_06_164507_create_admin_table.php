6152c4f04c6b9bf6c507435ed9735f54fbf4f8e1
Kauan Henrique <khds2013@gmail.com>
2021-02-18 07:36:52 -0300

    Criando novo campo na tabela de admin

--- database/migrations/2020_09_06_164507_create_admin_table.php
+++ database/migrations/2020_09_06_164507_create_admin_table.php
@@ -21,8 +21,10 @@ class CreateAdminTable extends Migration
             $table->string('TELEFONE',15);            
             $table->unsignedInteger('ID_USUARIO');
             $table->unsignedInteger('ID_ENDERECO');
+            $table->unsignedInteger('ID_FOTO')->nullable();
             $table->foreign('ID_USUARIO')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('ID_ENDERECO')->references('ID')->on('ENDERECOS')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('ID_FOTO')->references('ID')->on('FOTOS')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
