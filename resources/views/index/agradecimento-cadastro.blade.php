ea99803536d73a966090148b19e7c22fa9c99377
Kauan Henrique <khds2013@gmail.com>
2021-02-20 14:33:53 -0300

    Mensagem de agradecimento

--- resources/views/index/agradecimento-cadastro.blade.php
+++ resources/views/index/agradecimento-cadastro.blade.php
@@ -7,8 +7,10 @@
                 <div class="container text-center">
                     <div class="row justify-content-center">   
                         <div class="col-lg-8">
-                            <h1 class="page-header-title mb-3">Obrigado pelo cadastro</h1>
-                            <p class="page-header-text">Seus dados foram enviados para análise</p>
+                            <h1 class="page-header-title mb-3">Cadastro feito com sucesso.</h1>
+                            <p class="page-header-text">Seus dados foram recebidos pela nossa equipe e o resultado da análise será enviado em até 72 horas.
+
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -17,14 +19,14 @@
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
             </div>
         </header>
-        <section class="bg-white py-10">
+        {{-- <section class="bg-white py-10">
             <div class="container">
                 <p class="lead">Nossa equipe irá analisar minuciosamente os dados enviados, em até 72 horas enviaremos o resultado da análise</p>
             </div>
             <div class="svg-border-rounded text-dark">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
             </div>
-        </section>
+        </section> --}}
     </div>
 </div>
 @endsection
