939d2e3d0d2198958dc02cd0d4798f53c15eb228
Kauan Henrique <khds2013@gmail.com>
2021-03-01 07:47:30 -0300

    Mexendo na view

--- resources/views/pagamentos/index.blade.php
+++ resources/views/pagamentos/index.blade.php
@@ -24,6 +24,7 @@
                         <tr>
                             <th scope="col"><center>Cartão de crédito</center></th>
                             <th scope="col"><center>Bandeira</center></th>
+                            <th scope="col">Favorito?</th>
                             <th scope="col"></th>
                         </tr>
                     </thead>
@@ -58,44 +59,23 @@
                                    <center> {$solicitante->BANDEIRA}} <img src="https://img.icons8.com/color/40/000000/bank-card-back-side.png"/> </center>
                                 </td>                        
                             @endif
-                            <td><a class="btn btn-primary" data-toggle="modal" data-target="#modalCadastro" href=""> Editar </a></td>
+                            @if ($solicitante->PRINCIPAL === 1)
+                                <td>
+                                    <center> <img src="https://img.icons8.com/fluent/25/000000/star.png"/> </center>
+                                </td>
+                            @else
+                                <td></td>
+                            @endif
+                            <td>
+                                <a class="btn btn-primary" href=""> Editar </a>
+                                <a class="btn btn-danger" href=""> Desativar </a>
+                            </td>
                         </tr>                              
                         @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
-        <!-- DataTales Example -->
-        {{-- <div class="card shadow mb-4">
-            <div class="card-header py-3">
-                <div class="row">
-                    <div class="col-md-6">
-                        <h6 class="m-0 font-weight-bold text-primary padding-top-15">Recibos</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="card-body">
-                @csrf
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th scope="col">Prestador</th>
-                            <th scope="col">Data do pagamento</th>
-                            <th scope="col">Valor</th>
-                            <th scope="col">Comprovante</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">Kauan Henrique</th>
-                            <td>21/10/2019</td>
-                            <td>R$50,00</td>
-                            <td><img src="https://img.icons8.com/color/25/000000/pdf.png"/></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div> --}}
     </div>
 </div>
 <!-- Modal -->
@@ -109,51 +89,6 @@
                 </button>
             </div>
             <div class="modal-body">
-                {{-- <form name="cadastroCartao" id="cadastroCartao" method="post" action="">
-                    <div class="row margin-top-10">
-                        <div class="col">
-                            <input class="form-control"type="text" name="numeroCartao" id="numeroCartao" placeholder="Número do seu cartão" required"><br> 
-                        </div>
-                        <div class="col">
-                            <input class="form-control"type="text" name="cvv" id="cvv" placeholder="Código verificador (CVV) " required"><br> 
-                        </div>
-                    </div>
-                    <div class="row margin-top-10">
-                        <div class="col">
-                            <label for="banco" class="text-dark">Mes de validade</label><br>
-                            <select name="select" class="custom-select" required>
-                                <option value="janeiro">Janeiro</option> 
-                                <option value="fevereiro">Fevereiro</option> 
-                                <option value="marco">Março</option> 
-                                <option value="abril">Abril</option> 
-                                <option value="maio">Maio</option> 
-                                <option value="junho">Junho</option> 
-                                <option value="julho">Julho</option> 
-                                <option value="agosto">Agosto</option> 
-                                <option value="setembro">Setembro</option> 
-                                <option value="outubro">Outubro</option> 
-                                <option value="novembro">Novembro</option> 
-                                <option value="dezembro">Dezembro</option> 
-                            </select>                                         
-                        </div>
-                        <div class="col">
-                            <label for="tipoConta" class="text-dark">Ano de validade</label><br>
-                            <select name="select" class="custom-select" required>
-                                <option value="valor1">2022</option> 
-                                <option value="valor2">2023</option> 
-                                <option value="valor3">2024</option> 
-                                <option value="valor4">2025</option> 
-                                <option value="valor5">2026</option> 
-                                <option value="valor6">2027</option> 
-                                <option value="valor7">2028</option> 
-                                <option value="valor8">2029</option> 
-                                <option value="valor9">2030</option> 
-                            </select>                                         
-                        </div>
-                    </div>
-                    <br>
-                    <input class="btn btn-success" type="submit" value="Salvar">
-                </form> --}}
                 <form action="{{url('processPaymentValidation')}}" method="post" id="paymentForm">
                     @csrf
                     {{-- <h3>Detalhe do comprador</h3> --}}
@@ -205,17 +140,20 @@
                         </div>
                         <div class="row margin-top-10">
                             <div class="col">
-                            <label for="securityCode">Código de segurança</label>
-                            <input class="form-control" id="securityCode" data-checkout="securityCode" type="text"
-                                onselectstart="return false" onpaste="return false"
-                                oncopy="return false" oncut="return false"
-                                ondrag="return false" ondrop="return false" autocomplete=off onchange="catchCVV()">
+                                <label for="securityCode">Código de segurança</label>
+                                <input class="form-control" id="securityCode" data-checkout="securityCode" type="text"
+                                    onselectstart="return false" onpaste="return false"
+                                    oncopy="return false" oncut="return false"
+                                    ondrag="return false" ondrop="return false" autocomplete=off onchange="catchCVV()">
                             </div>
                             <div class="col" id="issuerInput">
-                                <label for="issuer">Banco emissor</label>
+                                <label for="issuer">Bandeira</label>
+                                <br>
                                 <select id="issuer" name="issuer" data-checkout="issuer"></select>
                             </div>
-                            <div class="col">
+                        </div>
+                        <div class="row margin-top-10">
+                            <div class="col d-none">
                                 <label for="installments">Parcelas</label>
                                 <select type="text" id="installments" name="installments"></select>
                             </div>
@@ -224,21 +162,18 @@
                                 <input type="hidden" name="cvv" id="cvv"/>
                                 <input type="hidden" name="paymentMethodId" id="paymentMethodId" />
                                 <input type="hidden" name="description" id="description" />
-                            <br>
-                            <br>
                             </div>
                         </div>
-                    <br>
-                    <div class="row margin-top-10">
-                        <div class="col">
-                            <label for="tipoConta">Favoritar cartão?</label><br>
-                            <input type="radio" name="cartaoPrincipal" id="cartaoPrincipal" value="1"> Sim<br> 
-                            <input type="radio" name="cartaoPrincipal" id="cartaoPrincipal" value="0"> Não<br> 
+                        <br>
+                        <div class="row margin-top-10">
+                            <div class="col">
+                                <label for="tipoConta">Favoritar cartão?</label><br>
+                                <input type="radio" name="cartaoPrincipal" id="cartaoPrincipal" value="1"> Sim<br> 
+                                <input type="radio" name="cartaoPrincipal" id="cartaoPrincipal" value="0"> Não<br> 
+                            </div>
                         </div>
-                    </div>
-                    <br>
-                    <br>
-                    <input class="btn btn-primary" type="submit" value="Salvar">
+                        <br>
+                        <input class="btn btn-primary" type="submit" value="Salvar">
                   </form>
             </div>
         </div>
