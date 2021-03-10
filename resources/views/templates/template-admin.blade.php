00f308ba806be29fc87a613602ae8eae4e9c150e
Kauan Henrique <kauan-henrique@kauan.henrique-Inspiron-3583>
2021-03-10 19:27:39 -0300

    Comentando coisas que não são utilizadas

--- resources/views/templates/template-admin.blade.php
+++ resources/views/templates/template-admin.blade.php
@@ -246,7 +246,7 @@
                         <ul class="navbar-nav ml-auto">
 
                             <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                            <li class="nav-item dropdown no-arrow d-sm-none">
+                            {{-- <li class="nav-item dropdown no-arrow d-sm-none">
                                 <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     <i class="fas fa-search fa-fw"></i>
                                 </a>
@@ -263,7 +263,7 @@
                                         </div>
                                     </form>
                                 </div>
-                            </li>
+                            </li> --}}
 
                             <!-- Nav Item - Alerts -->
                             {{-- <li class="nav-item dropdown no-arrow mx-1">
