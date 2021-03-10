d1294892f26010db0e68af545443ff5c0f72d4d2
Kauan Henrique <kauan-henrique@kauan.henrique-Inspiron-3583>
2021-03-10 19:37:01 -0300

    Criando as contantes com as credenciais

--- config/constants.php
+++ config/constants.php
@@ -15,5 +15,10 @@ return [
         'PENDENTE' => 3,
         'APROVADO' => 4,
         'CANCELADO' => 5,
+    ],
+
+    'TOKEN' => [
+        'PROD_ACCESS_TOKEN' => 'APP_USR-2933194983833876-020323-21f3edbc3d0d99c6a0307b59535daca1-200979127',
+        'PROD_PUBLICKEY_TOKEN' => 'APP_USR-0a3a3ea2-f003-4d88-8b81-13623a21f08b',
     ]
 ];
