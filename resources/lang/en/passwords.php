8996ec8670ca3f06bf9ebd6005af088de8004365
Kauan Henrique <kauan-henrique@kauan.henrique-Inspiron-3583>
2021-03-28 09:45:58 -0300

    alteracao simples

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -17,6 +17,6 @@ return [
     'sent' => 'Enviamos seu link de redefinição de senha por e-mail!',
     'throttled' => 'Aguarde antes de tentar novamente.',
     'token' => 'Este token de redefinição de senha é inválido.',
-    'user' => "Não conseguimos encontrar um usuário com esse endereço de e-mail. Entre com o nosso atendimento.",
+    'user' => "Não conseguimos encontrar um usuário com esse endereço de e-mail. Entre com o nosso atendimento pelo e-mail contato@wecares.com.br.",
 
 ];
