bded99c1a17f8e298fdf3bcef1a2a87257366e40
Kauan Henrique <kauan-henrique@kauan.henrique-Inspiron-3583>
2021-04-04 13:51:14 -0300

    Voltando codigo normal

--- app/Http/Controllers/PagamentosController.php
+++ app/Http/Controllers/PagamentosController.php
@@ -202,11 +202,10 @@ class PagamentosController extends Controller
 
         } else {
             $errorArray = (array)$payment->error;
-            Storage::put('file.txt', $errorArray);
             echo json_encode ($errorArray);
         }
 
-        // return redirect('/pagamentos');
+        return redirect('/pagamentos');
     }
 
     public function estornoPaymentValidation()
