c944ada28cd0b0463d734cd8f291e7bdb061c209
Kauan Henrique <khds2013@gmail.com>
2021-09-08 07:15:33 -0300

    Alteracao label

--- resources/views/prestadores/resultado-prestadores.blade.php
+++ resources/views/prestadores/resultado-prestadores.blade.php
@@ -52,7 +52,7 @@
             <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="modalServicoLabel">Informações da proposta</h5>
+                        <h5 class="modal-title" id="modalServicoLabel">Informações do pedido</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
                             <span aria-hidden="true">&times;</span>
                         </button>
